944d53a76679241c3e665d92cf6e0928a8483eee
= <983275852@qq.com>
2017-12-24 16:24:47 +0800

    update

--- synchronized/TestSynchronized/TestSynchronized/ViewController.mm
+++ synchronized/TestSynchronized/TestSynchronized/ViewController.mm
@@ -21,15 +21,57 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    self.ticketsCounts = 10;
-    self.sellThread1 = [[NSThread alloc] initWithTarget:self selector:@selector(sellWindow01) object:nil];
-    self.sellThread2 = [[NSThread alloc] initWithTarget:self selector:@selector(sellWindow02) object:nil];
+    self.ticketsCounts = 100;
+    
+    self.sellThread1 = [[NSThread alloc] initWithTarget:self selector:@selector(sellWindow01_NotThreadSelf) object:nil];
+    self.sellThread2 = [[NSThread alloc] initWithTarget:self selector:@selector(sellWindow02_NotThreadSelf) object:nil];
+    NSLog(@"开始卖票..");
     [self.sellThread1 start];
     [self.sellThread2 start];
+    
+//    self.sellThread1 = [[NSThread alloc] initWithTarget:self selector:@selector(sellWindow01) object:nil];
+//    self.sellThread2 = [[NSThread alloc] initWithTarget:self selector:@selector(sellWindow02) object:nil];
+//    [self.sellThread1 start];
+//    [self.sellThread2 start];
     
     
     self.ticketsLock = [[NSLock alloc] init];
 }
+
+- (void)sellWindow01_NotThreadSelf
+{
+    while (self.ticketsCounts) {
+
+        if (self.ticketsCounts == 0) {
+            [self.sellThread1 cancel];
+            NSLog(@"window01: 票卖完了... ");
+            return;
+        }
+        self.ticketsCounts -= 1;
+        NSLog(@"window01 卖了一张票,剩余票数：%d ",self.ticketsCounts);
+        usleep(1000*1000); // 休眠1s
+
+    }
+
+}
+
+- (void)sellWindow02_NotThreadSelf
+{
+    while (self.ticketsCounts) {
+        
+        if (self.ticketsCounts == 0) {
+            [self.sellThread1 cancel];
+            NSLog(@"window02: 票卖完了... ");
+            return;
+        }
+        self.ticketsCounts -= 1;
+        NSLog(@"window02 卖了一张票,剩余票数：%d ",self.ticketsCounts);
+        usleep(1000*1000); // 休眠1s
+        
+    }
+    
+}
+
 
 - (void)sellWindow01
 {
