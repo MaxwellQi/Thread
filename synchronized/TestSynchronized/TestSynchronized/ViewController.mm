ed0d6747576aa5b9abaa659f984f86592deeb00a
= <983275852@qq.com>
2017-12-24 17:21:23 +0800

    update

--- synchronized/TestSynchronized/TestSynchronized/ViewController.mm
+++ synchronized/TestSynchronized/TestSynchronized/ViewController.mm
@@ -14,25 +14,36 @@
 @property (nonatomic,strong) NSThread *sellThread2; // 卖票线程2
 
 @property (nonatomic,strong) NSLock *ticketsLock;
+
+@property (nonatomic,strong) dispatch_queue_t phoneQueue;
 @end
 
 @implementation ViewController
+
+- (dispatch_queue_t)phoneQueue
+{
+    if (!_phoneQueue) {
+        _phoneQueue = dispatch_queue_create("phone_queue", DISPATCH_QUEUE_SERIAL);
+    }
+    return _phoneQueue;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     self.ticketsCounts = 100;
     
-    self.sellThread1 = [[NSThread alloc] initWithTarget:self selector:@selector(sellWindow01_NotThreadSelf) object:nil];
-    self.sellThread2 = [[NSThread alloc] initWithTarget:self selector:@selector(sellWindow02_NotThreadSelf) object:nil];
+//    self.sellThread1 = [[NSThread alloc] initWithTarget:self selector:@selector(sellWindow01_NotThreadSelf) object:nil];
+//    self.sellThread2 = [[NSThread alloc] initWithTarget:self selector:@selector(sellWindow02_NotThreadSelf) object:nil];
+//    NSLog(@"开始卖票..");
+//    [self.sellThread1 start];
+//    [self.sellThread2 start];
+    
+    self.sellThread1 = [[NSThread alloc] initWithTarget:self selector:@selector(sellWindow01) object:nil];
+    self.sellThread2 = [[NSThread alloc] initWithTarget:self selector:@selector(sellWindow02) object:nil];
     NSLog(@"开始卖票..");
     [self.sellThread1 start];
     [self.sellThread2 start];
-    
-//    self.sellThread1 = [[NSThread alloc] initWithTarget:self selector:@selector(sellWindow01) object:nil];
-//    self.sellThread2 = [[NSThread alloc] initWithTarget:self selector:@selector(sellWindow02) object:nil];
-//    [self.sellThread1 start];
-//    [self.sellThread2 start];
     
     
     self.ticketsLock = [[NSLock alloc] init];
@@ -76,11 +87,11 @@
 - (void)sellWindow01
 {
 
-/** 同步代码块  **/
+///* 同步代码块  */
 //    @synchronized (self) {
-//        
+//
 //        while (self.ticketsCounts) {
-//            
+//
 //            if (self.ticketsCounts == 0) {
 //                [self.sellThread1 cancel];
 //                NSLog(@"window01: 票卖完了... ");
@@ -89,16 +100,33 @@
 //            self.ticketsCounts -= 1;
 //            NSLog(@"window01 卖了一张票,剩余票数：%d ",self.ticketsCounts);
 //            usleep(1000*1000); // 休眠1s
-//            
+//
 //        }
-//        
+//
 //    }
     
     
 /** 加锁 **/
-    [self.ticketsLock lock];
+//    [self.ticketsLock lock];
+//        while (self.ticketsCounts) {
+//
+//            if (self.ticketsCounts == 0) {
+//                [self.sellThread1 cancel];
+//                NSLog(@"window01: 票卖完了... ");
+//                return;
+//            }
+//            self.ticketsCounts -= 1;
+//            NSLog(@"window01 卖了一张票,剩余票数：%d ",self.ticketsCounts);
+//            usleep(1000*1000); // 休眠1s
+//
+//        }
+//    [self.ticketsLock unlock];
+    
+    
+    /** 放入串行队列中 **/
+    dispatch_async(self.phoneQueue, ^{
         while (self.ticketsCounts) {
-
+            
             if (self.ticketsCounts == 0) {
                 [self.sellThread1 cancel];
                 NSLog(@"window01: 票卖完了... ");
@@ -109,18 +137,18 @@
             usleep(1000*1000); // 休眠1s
             
         }
-    [self.ticketsLock unlock];
+    });
     
 
 }
 
 - (void)sellWindow02
 {
-/** 同步代码块  **/
+///** 同步代码块  **/
 //    @synchronized (self) {
-//        
+//
 //        while (self.ticketsCounts) {
-//            
+//
 //            if (self.ticketsCounts == 0) {
 //                [self.sellThread2 cancel];
 //                NSLog(@"window02: 票卖完了... ");
@@ -129,17 +157,33 @@
 //            self.ticketsCounts -= 1;
 //            NSLog(@"window02 卖了一张票,剩余票数：%d ",self.ticketsCounts);
 //            usleep(1000*1000); // 休眠1s
-//            
-//        }
 //
-//        
+//        }
 //    }
     
 
     
     
- /** 加锁 **/
-        [self.ticketsLock lock];
+// /** 加锁 **/
+//        [self.ticketsLock lock];
+//        while (self.ticketsCounts) {
+//
+//            if (self.ticketsCounts == 0) {
+//                [self.sellThread2 cancel];
+//                NSLog(@"window02: 票卖完了... ");
+//                return;
+//            }
+//            self.ticketsCounts -= 1;
+//            NSLog(@"window02 卖了一张票,剩余票数：%d ",self.ticketsCounts);
+//            usleep(1000*1000); // 休眠1s
+//
+//        }
+//        [self.ticketsLock unlock];
+    
+    
+    /** 放入串行队列中 **/
+    
+    dispatch_async(self.phoneQueue, ^{
         while (self.ticketsCounts) {
 
             if (self.ticketsCounts == 0) {
@@ -150,9 +194,9 @@
             self.ticketsCounts -= 1;
             NSLog(@"window02 卖了一张票,剩余票数：%d ",self.ticketsCounts);
             usleep(1000*1000); // 休眠1s
-            
         }
-        [self.ticketsLock unlock];
+        
+    });
     
 
 }
